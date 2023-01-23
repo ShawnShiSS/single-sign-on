e5968345c6bcf4ca60b91da83f26a5e8a83a5828
Shawn Shi <ss.shawnshi@gmail.com>
2023-01-23 11:20:24 -0500

    Add additional user claims

--- src/SsoServer/Models/ApplicationUser.cs
+++ src/SsoServer/Models/ApplicationUser.cs
@@ -6,8 +6,15 @@ using Microsoft.AspNetCore.Identity;
 
 namespace SsoServer.Models
 {
-    // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        // Add profile data for application users by adding properties to the ApplicationUser class, see example below.
+
+        /// <summary>
+        ///     Whether the account is enabled/disabled.  
+        ///     This is different than the user being locked out due to using an incorrect password too many times.
+        /// </summary>
+        //public bool IsEnabled { get; set; }
+
     }
 }
