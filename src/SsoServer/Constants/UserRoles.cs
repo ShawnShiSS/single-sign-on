13658a46442c9d450dd3fd9cf7f23903995da637
Shawn Shi <ss.shawnshi@gmail.com>
2023-01-23 11:09:28 -0500

    Define user roles

---
+++ src/SsoServer/Constants/UserRoles.cs
@@ -0,0 +1,14 @@
+﻿namespace SsoServer.Constants
+{
+    public class UserRoles
+    {
+        /// <summary>
+        /// Administrator role
+        /// </summary>
+        public static readonly string Administrator = "Administrator";
+        /// <summary>
+        /// Finance role
+        /// </summary>
+        public static readonly string Finance = "Finance";
+    }
+}
