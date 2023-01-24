6c39d9dd7dabb8a369b1e3ec9d3b949abf7a0dd3
Shawn Shi <ss.shawnshi@gmail.com>
2023-01-24 11:17:14 -0500

    Add IsEnabled flag for AspNetCore Identity User

--- src/SsoServer/Data/Migrations/AspNetCoreIdentity/20230124161450_AddIsEnabled.Designer.cs
+++ src/SsoServer/Data/Migrations/AspNetCoreIdentity/20230124161450_AddIsEnabled.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SsoServer.Data;
 
@@ -10,9 +11,10 @@ using SsoServer.Data;
 namespace SsoServer.Data.Migrations.AspNetCoreIdentity
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230124161450_AddIsEnabled")]
+    partial class AddIsEnabled
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.0");
