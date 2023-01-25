1b819a12854050cc9741f7083d39b5f24c072313
Shawn Shi <ss.shawnshi@gmail.com>
2023-01-24 21:48:28 -0500

    Remove property from Identity User

--- src/SsoServer/Models/ApplicationUser.cs
+++ src/SsoServer/Models/ApplicationUser.cs
@@ -10,11 +10,7 @@ namespace SsoServer.Models
     {
         // Add profile data for application users by adding properties to the ApplicationUser class, see example below.
 
-        /// <summary>
-        ///     Whether the account is enabled/disabled.  
-        ///     This is different than the user being locked out due to using an incorrect password too many times.
-        /// </summary>
-        public bool IsEnabled { get; set; }
+        
 
     }
 }
