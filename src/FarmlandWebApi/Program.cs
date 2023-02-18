ee1420fae6c7582001b4432657f20288a5d8a9bc
Shawn Shi <ss.shawnshi@gmail.com>
2023-02-17 21:14:53 -0500

    Add JWT authentication and authorization policy.

--- src/FarmlandWebApi/Program.cs
+++ src/FarmlandWebApi/Program.cs
@@ -1,9 +1,39 @@
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Configure token authentication
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        // This is the URL for our authentication server
+        options.Authority = "https://localhost:5001";
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+
+// Authorization policy to require an API scope called farmlandwebapi in the JWT token.
+// "farmlandwebapi" should be the same value defined in authentication server, see farmlandwebapi in IdentityServerConfigurationDbSeeder.cs.
+// Without this, the API would accept any access token issued by the authentication server.
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("RequiresFarmlandWebApiApiScope", policy =>
+    {
+        // Requires the user to be authenticated
+        policy.RequireAuthenticatedUser();
+        // Requires the user to have access to the farmland web api.
+        policy.RequireClaim("scope", "farmlandwebapi");
+    });
+});
+
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -18,8 +48,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Requires token authentication configured above
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
+
+app.MapControllers()
+   // Apply the authorization policy globally
+   .RequireAuthorization("RequiresFarmlandWebApiApiScope");
 
 app.Run();
